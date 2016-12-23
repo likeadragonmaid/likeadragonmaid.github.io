de77ff6bb8ecb29bf9eac8bb224cd4163b05b9d7
Karanvir Singh <karanvir.singh61@yahoo.in>
2016-12-23 20:29:11 +0530

    Fixing stuff

--- jsdelivr/widget.js
+++ jsdelivr/widget.js
@@ -1,4 +1,4 @@
-(function(e){var t="http://lab.lepture.com/github-cards/";
+(function(e){var t="//cdn.jsdelivr.net/github-cards/1.0.2/";
 var r,i=0;
 var a=e.getElementsByTagName("meta");
 var n,d,l,c;
